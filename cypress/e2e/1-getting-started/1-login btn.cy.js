b6c5ef46aa79d21a3cd73b505febdd9fcf897b34
Zain <rana.zain909@gmail.com>
2022-06-02 12:17:09 +0500

    testing of login button on homepage

---
+++ cypress/e2e/1-getting-started/1-login btn.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+ 
+describe('On HomePage',()=>{
+  it('visiting homepage', () => {
+      cy.visit('http://localhost:19006/')
+  })
+  
+  it("pressing login btn", ()=>{
+    
+    cy.get('#root > div > div > div > div > div > div > div > div.css-view-1dbjc4n.r-bottom-1p0dtai.r-left-1d2f490.r-pointerEvents-12vffkv.r-position-u8s1d.r-right-zchlnj.r-top-ipm5af > div.css-view-1dbjc4n.r-flex-13awgt0.r-pointerEvents-12vffkv > div > div > div > div.css-view-1dbjc4n.r-flex-13awgt0 > div > div.css-view-1dbjc4n.r-alignItems-1awozwy.r-alignSelf-1kihuf0.r-flex-13awgt0.r-justifyContent-1777fci.r-maxWidth-131bt2l.r-padding-1pcd2l5.r-width-13qz1uu > div:nth-child(4) > div > div').click('center')
+  
+  })
+  
+  it('pressing back btn', ()=>{
+    cy.get('#root > div > div > div > div > div > div > div:nth-child(2) > div.css-view-1dbjc4n.r-bottom-1p0dtai.r-left-1d2f490.r-pointerEvents-12vffkv.r-position-u8s1d.r-right-zchlnj.r-top-ipm5af > div.css-view-1dbjc4n.r-flex-13awgt0.r-pointerEvents-12vffkv > div > div > div > div.css-view-1dbjc4n.r-flex-13awgt0 > div > div.css-view-1dbjc4n.r-alignItems-1awozwy.r-alignSelf-1kihuf0.r-flex-13awgt0.r-justifyContent-1777fci.r-maxWidth-131bt2l.r-padding-1pcd2l5.r-width-13qz1uu > div.css-view-1dbjc4n.r-cursor-1loqt21.r-left-ws9h79.r-position-u8s1d.r-top-1ki14p2.r-touchAction-1otgn73.r-transitionProperty-1i6wzkk.r-userSelect-lrvibr > div > img').click('topLeft')
+  })
+  
+}) 
